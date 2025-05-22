88497a430baad834b1b2f84f975b96374aa54cfc
zuldanshaxshax <mascuudcade3030@gmail.com>
2025-05-22 18:08:05 +0300

    Update axios.js

--- portal/src/lib/axios.js
+++ portal/src/lib/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:6400/api/v1",
+  baseURL: "https://fireemergency.up.railway.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
